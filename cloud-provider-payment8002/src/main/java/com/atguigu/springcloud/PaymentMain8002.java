4846b5f1c431ccb3a23a47bab750e4bdef6307e8
whiteAmbition <baizhaohui_kd@163.com>
2021-12-16 09:43:35 +0800

    ribbon和openfeign

--- cloud-provider-payment8002/src/main/java/com/atguigu/springcloud/PaymentMain8002.java
+++ cloud-provider-payment8002/src/main/java/com/atguigu/springcloud/PaymentMain8002.java
@@ -1,5 +1,8 @@
 package com.atguigu.springcloud;
 
+import com.netflix.client.IClient;
+import com.netflix.loadbalancer.ILoadBalancer;
+import com.netflix.loadbalancer.ZoneAwareLoadBalancer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -13,5 +16,8 @@ public class PaymentMain8002 {
 
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8002.class,args);
+//        Client client=new LoadBalancerFeignClient();
+//        IClient iClient = new FeignLoadBalancer();
+//        ILoadBalancer iLoadBalancer=new ZoneAwareLoadBalancer<>();
     }
 }
